a2dcf54139fc235f1670bf4ecf5ba7eb23d4a2ee
Renaud Racinet <renauddev@macbook-pro-de-renaud.home>
2022-05-20 18:17:35 +0200

    Feat(Product): Add Data and show first button Product.scss: Add heading css class App.scss: Import new stylesheet and add diabled cursor Product: Add Data, display/align in table and create button for the future features

--- src/components/hooks/Product.tsx
+++ src/components/hooks/Product.tsx
@@ -1,14 +1,35 @@
-import {FC, Fragment, useState} from "react";
+import {FC, Fragment, useEffect, useState} from "react";
 import {Button, Table} from "react-bootstrap";
 import ProductModel from '../../model/Product';
 
 const Products: FC<any> = () => {
 
     const [products, setProducts] = useState<Array<ProductModel>>([]);
+    const [error, setError] = useState<string|null>(null);
+
+    useEffect( () => {
+        fetch('http://localhost:3000/product', {
+            method: "get"
+        }).then( response => response.json())
+            .then( data => setProducts(data))
+            .catch( err => setError(err) );
+    }, []);
 
     return (
         <Fragment>
-            <h1>Product</h1>
+            <section className="heading">
+                <h1>Product</h1>
+                <div className="btn-group ">
+                    <Button variant="primary" size="sm" disabled>
+                        Create new one
+                    </Button>
+                    <Button variant="secondary"  size="sm" disabled>
+                        Create Many
+                    </Button>
+                </div>
+
+            </section>
+
             <Table>
                 <thead>
                 <tr>
@@ -27,19 +48,19 @@ const Products: FC<any> = () => {
                     products?.map( product =>
                         (
                             <tr key={product?.id}>
-                                <td>product?.id</td>
-                                <td>product?.name</td>
-                                <td>product?.description</td>
-                                <td>product?.picture</td>
-                                <td>product?.price</td>
-                                <td>product?.stock</td>
-                                <td>product?.available</td>
-                                <td>
+                                <td className="align-middle">{product?.id}</td>
+                                <td className="align-middle">{product?.name}</td>
+                                <td className="text-truncate align-middle">{product?.description.slice(0, 95)}</td>
+                                <td className="align-middle"><img src={product?.picture} alt={"picture of " + product?.name} width="100" height="50" /></td>
+                                <td className="align-middle">{product?.price}</td>
+                                <td className="align-middle">{product?.stock}</td>
+                                <td className="align-middle">{ (product?.isAvailable) ? "Yes" : "No" }</td>
+                                <td className="align-middle">
                                     <div className="btn-group">
-                                        <Button variant="primary">
+                                        <Button variant="primary" disabled>
                                             See
                                         </Button>
-                                        <Button variant="danger">
+                                        <Button variant="danger" disabled>
                                             Delete
                                         </Button>
                                     </div>
